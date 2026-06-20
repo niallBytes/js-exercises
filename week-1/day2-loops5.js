47fc181f457267c20c65984da85e9f9c46fc9149
niallBytes <unaniniall3@gmail.com>
2026-06-20 16:11:13 +0530

    Day 2 - Js Revival - Loops Exercise 5

---
+++ week-1/day2-loops5.js
@@ -0,0 +1,16 @@
+// ============================================================
+// EXERCISE — Replace "for" with "while"
+// ============================================================
+
+/* Q: Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).
+
+for (let i = 0; i < 3; i++) {
+  alert( `number ${i}!` );
+} 
+*/
+
+let i = 0;
+while(i < 3) {
+    console.log(`Number ${i}`);
+    i++;
+}
