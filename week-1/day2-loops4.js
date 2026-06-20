426221813c5a13a85d7c2c32e65e5d48f48437de
niallBytes <unaniniall3@gmail.com>
2026-06-20 16:04:59 +0530

    Day 2 - Js Revival - Loops Exercise 4

---
+++ week-1/day2-loops4.js
@@ -0,0 +1,11 @@
+// ============================================================
+// EXERCISE — Output even numbers in the loop
+// ============================================================
+
+// Q: Use the for loop to output even numbers from 2 to 10.
+
+for (let i = 2; i <= 10; i++) {
+    if(i%2 != 0) continue
+
+    console.log(i)
+};
